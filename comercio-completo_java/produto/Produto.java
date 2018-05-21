023d55571613722d93b8b8e158134e5689e1c2e1
Isidore Petersen <39338469+isidorefilmes@users.noreply.github.com>
2018-05-21 16:44:36 -0300

    Add files via upload

---
+++ comercio-completo_java/produto/Produto.java
@@ -0,0 +1,99 @@
+/*Isidore Petersen RELEASE
+
+ * LICENSE: This source file is subject to Attibution version 2.5 Brazil of the Creative Commons
+ * license that is available through the following URI:  http://hack.isiflix.com
+ * If you did not receive a copy of this license and are unable to obtain it through the web, please
+ * send a note to the authors so they can mail you a copy immediately.
+ * Peterson Isidoro a.k.a Isidore Petersen
+ *
+ */
+
+package com.livro.capitulo3.produto;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+	private static final long	serialVersionUID	= -3445365595630310499L;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "cod_produto")
+	private Integer						produto;
+
+	@Column(name = "descricao")
+	private String						descricao;
+
+	@Column(name = "preco")
+	private Double						preco;
+
+	public Integer getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Integer produto) {
+		this.produto = produto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Produto other = (Produto) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		return true;
+	}
+
+}
---
+++ comercio-completo_java/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+/*Isidore Petersen RELEASE
+
+ * LICENSE: This source file is subject to Attibution version 2.5 Brazil of the Creative Commons
+ * license that is available through the following URI:  http://hack.isiflix.com
+ * If you did not receive a copy of this license and are unable to obtain it through the web, please
+ * send a note to the authors so they can mail you a copy immediately.
+ * Peterson Isidoro a.k.a Isidore Petersen
+ *
+ */
+package com.livro.capitulo3.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory	sessionFactory	= buildSessionFactory();
+  
+	private static SessionFactory buildSessionFactory() {
+    try {
+      AnnotationConfiguration cfg = new AnnotationConfiguration();
+      cfg.configure("hibernate.cfg.xml");
+      return cfg.buildSessionFactory();
+    } catch (Throwable e) {
+    	System.out.println("Cria��o inicial do objeto SessionFactory falhou. Erro: " + e);
+			throw new ExceptionInInitializerError(e);
+    }
+  }
+  
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
